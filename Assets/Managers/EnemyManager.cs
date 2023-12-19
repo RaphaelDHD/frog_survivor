28867d99750a2e00873021d29fff86cd053a370a
DECHENAUD RAPHAEL p2106803 <raphael.dechenaud@etu.univ-lyon1.fr>
2023-12-19 15:05:59 +0100

    Ajout du mouvement des ennemies et du nombre qu'il peut y avoir en même temps

--- Assets/Managers/EnemyManager.cs
+++ Assets/Managers/EnemyManager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
@@ -8,10 +7,11 @@ public class EnemyManager : MonoBehaviour
     private BoxCollider spawnBounds = null;
     public Vector2 spawnTime = Vector2.zero;
     public float spawnHeight = 1.0f;
-    public float minDistance = 5.0f;
     public GameObject spawnObject = null;
-
     public GameObject player = null;
+
+    private int enemyCount = 0;
+
 
 
     void Start()
@@ -25,48 +25,51 @@ public class EnemyManager : MonoBehaviour
         spawner.transform.position = player.transform.position;
     }
 
-
     private IEnumerator SpawnObject()
     {
         float randSpawnTime = Random.Range(spawnTime.x, spawnTime.y);
         yield return new WaitForSeconds(randSpawnTime);
 
-        bool farFromPlayer = false;
-        Vector3 spawnPos = Vector3.zero;
-        while (!farFromPlayer)
+
+        while (enemyCount >= 5)
         {
-            // Choose a random edge of the spawnBounds (left, right, top, or bottom)
-            int edgeIndex = Random.Range(0, 4); // 0: left, 1: right, 2: top, 3: bottom
-
-            float xSpawn = 0f, zSpawn = 0f;
-
-            switch (edgeIndex)
-            {
-                case 0: // left
-                    xSpawn = spawnBounds.bounds.min.x;
-                    zSpawn = Random.Range(spawnBounds.bounds.min.z, spawnBounds.bounds.max.z);
-                    break;
-                case 1: // right
-                    xSpawn = spawnBounds.bounds.max.x;
-                    zSpawn = Random.Range(spawnBounds.bounds.min.z, spawnBounds.bounds.max.z);
-                    break;
-                case 2: // top
-                    xSpawn = Random.Range(spawnBounds.bounds.min.x, spawnBounds.bounds.max.x);
-                    zSpawn = spawnBounds.bounds.max.z;
-                    break;
-                case 3: // bottom
-                    xSpawn = Random.Range(spawnBounds.bounds.min.x, spawnBounds.bounds.max.x);
-                    zSpawn = spawnBounds.bounds.min.z;
-                    break;
-            }
-
-            spawnPos = new Vector3(xSpawn, spawnHeight, zSpawn);
-            farFromPlayer = Vector3.Distance(spawnPos, player.transform.position) > minDistance;
-
+            yield return new WaitForSeconds(1.0f);
         }
 
+        Vector3 spawnPos = GetRandomEdgePosition();
         Instantiate(spawnObject, spawnPos, Quaternion.identity);
+        enemyCount++;
         StartCoroutine(SpawnObject());
     }
 
+
+
+
+    private Vector3 GetRandomEdgePosition()
+    {
+        int edgeIndex = Random.Range(0, 4); // 0: left, 1: right, 2: top, 3: bottom
+        float xSpawn = 0f, zSpawn = 0f;
+
+        switch (edgeIndex)
+        {
+            case 0: // left
+                xSpawn = spawnBounds.bounds.min.x;
+                zSpawn = Random.Range(spawnBounds.bounds.min.z, spawnBounds.bounds.max.z);
+                break;
+            case 1: // right
+                xSpawn = spawnBounds.bounds.max.x;
+                zSpawn = Random.Range(spawnBounds.bounds.min.z, spawnBounds.bounds.max.z);
+                break;
+            case 2: // top
+                xSpawn = Random.Range(spawnBounds.bounds.min.x, spawnBounds.bounds.max.x);
+                zSpawn = spawnBounds.bounds.max.z;
+                break;
+            case 3: // bottom
+                xSpawn = Random.Range(spawnBounds.bounds.min.x, spawnBounds.bounds.max.x);
+                zSpawn = spawnBounds.bounds.min.z;
+                break;
+        }
+
+        return new Vector3(xSpawn, spawnHeight, zSpawn);
+    }
 }
